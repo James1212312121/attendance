711f008f081ff23ed6e16918934352fd1619d2e5
James1212312121 <jamespanupatr@gmail.com>
2020-02-19 13:55:19 -1000

    add flask file

---
+++ attendanceFlask.py
@@ -0,0 +1,69 @@
+import mysql.connector
+import RPi.GPIO as GPIO
+from time import sleep
+from flask import Flask
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user="root",
+    passwd = "password",
+    database = "school"
+)
+
+mycursor = mydb.cursor()
+
+text ='''
+<t1>
+Attendance
+<t1>
+
+<br>
+
+<table style="width:100%; border: 2px solid black;">
+    <tr>
+        <th> ID </th>
+        <th> Student Name </th>
+        <th> Class Name </th>
+        <th> Class Teacher </th>
+        <th> Attendance </th>
+    </tr>
+'''
+
+@app.route('/')
+def hello_world():
+    return text + "</table>"
+
+
+mycursor.execute('select * from attendance')
+myresult = mycursor.fetchall()
+for x in myresult:
+    mycursor.execute('select nick_name from students where id = ' + str(x[0]))
+    myresult2 = mycursor.fetchall()
+    for y in myresult2:
+        name = str(y[0])
+        
+    mycursor.execute('select * from classes where id = ' + str(x[1]))
+    myresult3 = mycursor.fetchall()
+    for z in myresult3:
+        classname = str(z[1])
+        classteacher = str(z[2])
+        
+    text += '''
+    <tr>
+        <th> ''' + str(x[0]) + '''</th>
+        <th> ''' + name + '''</th>
+        <th> ''' + classname + '''</th>
+        <th> ''' + classteacher + '''</th>
+        <th> ''' + str(x[2]) + '''</th>
+    </tr>
+
+    '''
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
